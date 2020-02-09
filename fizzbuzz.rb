beb74dacff862128d65a930a6158c51a3b916f4f
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 05:30:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 3
     return  "Fizz"
   end
 end
