31c976efb632a3c7f07a1a00c01d29b4ada31e46
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 05:28:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(fizz_number)
+def fizzbuzz(int)
   if fizz_number % 3 == 0
      "Fizz"
   end
