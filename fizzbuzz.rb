ef66a1443330260e2bf1c9d1af219351b3f6955f
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 05:34:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  num = int % 3
+  puts num
   if int / 3 == 9
     return  "Fizz"
   end
