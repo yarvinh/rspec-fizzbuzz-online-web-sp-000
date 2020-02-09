b90a3f01cb4b2a5a0b8122842d1feb8b8010eccd
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 04:46:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(fizz_number)
-  if fizz_number / 9
+  if 9 / fizz_number
     puts "Fizz"
  "Fizz"
   end
