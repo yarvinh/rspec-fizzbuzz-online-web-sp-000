58c48e2c3cf6be7f58d468109a2a7ce8b7f6d017
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 06:00:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,10 +6,9 @@ def fizzbuzz(int)
     return  "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  else
-    nil
+
   end
 end
 #fizzbuzz(3)
