617c99c044d242a0589b9d2d022bcb28eb88e608
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 06:10:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  if int % 3 &&  % 5 == 0
+    puts "fizzbuzz"
+    "FizzBuzz"
 
-  if int % 3 == 0
+elsif int % 3 == 0
     puts "fizz"
     return  "Fizz"
   elsif int % 5 == 0
     puts "buzz"
     "Buzz"
-  elsif int % 3 &&  % 5 == 0
-    puts "fizzbuzz"
-    "FizzBuzz"
+  # elsif int % 3 &&  % 5 == 0
+  #   puts "fizzbuzz"
+  #   "FizzBuzz"
 
   end
 end
